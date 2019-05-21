674c3abc30f4305ba601811b43ad3d9e00b50c96
宋静 <song.jing@iinda.cn>
2019-05-21 11:20:32 +0800

    深拷贝

--- js/deepCopy.js
+++ js/deepCopy.js
@@ -1,4 +1,29 @@
+/**
+ * Created by sumily on 2019/5/21.
+ * Version:
+ * Description:深拷贝的几种情况
+ */
 
+
+var obj = {
+    test1:null,
+    test2:undefined,
+    test3:[1,2,3],
+    test4:[{t1:1,t2:2},{t1:1,t2:'2'}],
+    add:function(a,b){
+        return a+b;
+    },
+    name: 'lyl',
+    age: 18
+};
+var obj1=[1,2,3,3,4,5];
+console.log(obj,obj1);
+
+/**
+ * 简单深拷贝，空，数组拷贝后类型全变成了对象
+ * @param source
+ * @returns {*}
+ */
 var deepCopy1 = function (source) {
     var result = {};
     for (var key in source) {
@@ -7,11 +32,34 @@ var deepCopy1 = function (source) {
     return result;
 };
 
+console.log("deepCopy1:");
+console.log(deepCopy1(obj));
+console.log(deepCopy1(obj1));
 
+
+/**
+ * 简单深拷贝2，对象中有函数的情况是不会拷贝的
+ * @param source
+ * @returns {*}
+ */
+var deepCopy2 = function (source) {
+    var _obj = JSON.stringify(source),
+        objClone = JSON.parse(_obj);
+    return objClone
+};
+
+console.log("deepCopy2:");
+console.log(deepCopy2(obj));
+console.log(deepCopy2(obj1));
+
+/**
+ * 考虑了空，数组，对象拷贝的情况
+ * @param source
+ * @returns {*}
+ */
 function deepCopy(source) {
     var result=Array.isArray(source) ? [] : {};
     for(var key in source){
-        // result[key]=typeof source[key]=='object'?deepCopy(source[key]):source[key];
         if(source[key]==null||typeof source[key]!='object'){
             result[key]=source[key];
         }else if(typeof source[key]=='object'){
@@ -24,35 +72,6 @@ function deepCopy(source) {
     }
     return result;
 }
-
-
-
-var obj={
-    test:undefined,
-    solar:null,
-    member:['song','li','momo'],
-    grade:1,
-    add:function (a,b) {
-        return a+b;
-    },
-    car:{
-        color:'red',
-        type:'jeep',
-        run:function (v) {
-            console.log(v);            }
-    }
-};
-console.log(obj);
-
-
-var arrObj=["lslsl","sss","dddd"];
-var _arrObj=deepCopy(arrObj);
-console.log(_arrObj);
-
-var _obj=deepCopy(obj);
-console.log(_obj);
-var _obj1=deepCopy1(obj);
-console.log(_obj1);
-
-
-console.log(typeof [1,2,3],typeof null);
+console.log("deepCopy:");
+console.log(deepCopy(obj));
+console.log(deepCopy(obj1));
